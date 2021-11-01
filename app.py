4fe9fd25e7ba009b1ff76f0401d04a391ddfc974
Marissa Patti <marissa.patti@thrivebio.com>
2021-11-01 15:41:43 -0400

    Added the rest of the pages to the navbar

--- app.py
+++ app.py
@@ -10,17 +10,17 @@ def index():
 def basicFunctions():
     return render_template('basicFunctions.html')
 
-# @app.route('/advancedFunctions')
-# def advancedFunctions():
-#     return render_template('advancedFunctions.html')
+@app.route('/advancedFunctions/')
+def advancedFunctions():
+    return render_template('advancedFunctions.html')
 
-# @app.route('/musicSync')
-# def musicSync():
-#     return render_template('musicSync.html')
+@app.route('/musicSync/')
+def musicSync():
+    return render_template('musicSync.html')
 
-# @app.route('/info')
-# def info():
-#     return render_template('info.html')
+@app.route('/info/')
+def info():
+    return render_template('info.html')
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=80)
