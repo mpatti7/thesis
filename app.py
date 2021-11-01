8b953c91025b6934cd0d7a8ea8f9fcd7c284916c
mpatti7 <mpatt967@mail.endicott.edu>
2021-10-31 21:57:13 -0400

    Created 2 pages for home and basics. Started making nav bar for the pages

--- app.py
+++ app.py
@@ -6,5 +6,21 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/basicFunctions/')
+def basicFunctions():
+    return render_template('basicFunctions.html')
+
+# @app.route('/advancedFunctions')
+# def advancedFunctions():
+#     return render_template('advancedFunctions.html')
+
+# @app.route('/musicSync')
+# def musicSync():
+#     return render_template('musicSync.html')
+
+# @app.route('/info')
+# def info():
+#     return render_template('info.html')
+
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=80)
